2fea9bf66042b1964a06b8e49ee6a1f64b1de3e4
Rahul Singh <thesincererahul@gmail.com>
2025-12-25 01:25:39 +0530

    start learning

--- main.dart
+++ main.dart
@@ -1,3 +1,13 @@
 void main () {
   print('Hello, World!');
+
+  var numbers = [1, 2, 3, 4, 5];
+
+  print('numbers: $numbers');
+
+  for (var i = 0; i < numbers.length; i++) {
+    numbers[i] = numbers[i] * 2;
+  }
+
+  print('numbers: $numbers');
 }
